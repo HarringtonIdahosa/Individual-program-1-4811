e66fb921b53bafc852dbfad7518c79bb3f6df697
HarringtonIdahosa <81630373+HarringtonIdahosa@users.noreply.github.com>
2024-03-11 11:17:06 -0400

    Update fork-join.c

--- fork-join.c
+++ fork-join.c
@@ -1,43 +1,33 @@
+#include <semaphore.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
-#include <semaphore.h>
 #include "common_threads.h"
 
-sem_t *s;  // Use a pointer for the semaphore
+sem_t s; 
 
 void *child(void *arg) {
     printf("child\n");
     // Signal that the child has finished
-    sem_post(s);
+    Sem_post(&s);
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
     pthread_t p;
 
-    // Create a named semaphore
-    s = sem_open("/my_semaphore", O_CREAT, 0644, 0);
-
-    if (s == SEM_FAILED) {
-        perror("sem_open");
-        return 1;
-    }
+    // Initialize semaphore with value 0
+    Sem_init(&s, 0);
 
     printf("parent: begin\n");
-
+    
     // Create child thread
-    pthread_create(&p, NULL, child, NULL);
+    Pthread_create(&p, NULL, child, NULL);
 
     // Wait for the child to finish
-    sem_wait(s);
+    Sem_wait(&s);
 
     printf("parent: end\n");
 
-    // Close and unlink the semaphore
-    sem_close(s);
-    sem_unlink("/my_semaphore");
-
     return 0;
 }
-
